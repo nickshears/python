036445abe353aa5678e015afda466281f781fc13
Nick <nickshears@live.co.uk>
2014-10-31 16:34:18 +0000

    Update number_guesser.py

--- number_guesser.py
+++ number_guesser.py
@@ -1,30 +1,40 @@
 import random
 
-# generate a random number
-correct_number = random.randint(0, 100)
+play_again = True;
 
-# fake a guessed number that's wrong so the loop can start
-guessed_number = None
+while (play_again):
 
-while (guessed_number != correct_number):
+	# generate a random number
+	correct_number = random.randint(0, 100)
 
-	guessed_number = int(input("Take a guess"))
+	# fake a guessed number that's wrong so the loop can start
+	guessed_number    = None
+	number_of_guesses = 0
+	
+	while (guessed_number != correct_number):
 
-	# calculate the distance between the 2 numbers first to avoid long conditions
-	if correct_number>guessed_number:
-		difference = correct_number-guessed_number
-	else:
-		difference = guessed_number-correct_number
+		guessed_number = int(input("Try and guess the correct number between 0 and 100"))
+		number_of_guesses = number_of_guesses+1
 
-
-	if (difference > 10):
-		print("cold")
-	else:
-
-		if (difference >= 5 and difference <= 10):
-			print("warm")
+		# calculate the distance between the 2 numbers first to avoid long conditions
+		if correct_number>guessed_number:
+			difference = correct_number-guessed_number
 		else:
-			# it can only be hot now
-			print("hot")
+			difference = guessed_number-correct_number
 
-print("your guess is correct!")
+		if (difference > 10):
+			print("cold")
+		else:
+			if (difference >= 5 and difference <= 10):
+				print("warm")
+			else:
+				# it can only be hot now
+				print("hot")
+
+	print("you took {0} guesses to get the correct number of {1}".format(number_of_guesses, correct_number))
+
+	decision = input("play again? y/n")
+
+	if (decision != 'y'):
+
+		play_again = False
