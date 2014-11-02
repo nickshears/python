fd423e53cbe14e9d81a66d2725aeaf512e75bb1a
Nick Shears <nickshears@live.co.uk>
2014-11-02 16:30:43 +0000

    update

--- 4-worksheet-exercise-3-number-of-vowels.py
+++ 4-worksheet-exercise-3-number-of-vowels.py
@@ -1,5 +1,5 @@
-# define a list of vowels with a corresponding count
-vowels = {'a':10, 'e':11, 'i':15, 'o':17, 'u':3}
+# define a dictionary of vowels with a corresponding count
+vowels = {'a':0, 'e':0, 'i':0, 'o':0, 'u':0}
 
 # define a word anything other than stop to start the loop
 word = 'start'
@@ -11,15 +11,16 @@ while (word != "stop"):
 	# loop through each letter in the word
 	for letter in word:
 
-		# lowercase each letter the user has input to see if its a vowel
+		# lowercase each letter the user has input to see if its a vowel in the dictionary
 		low_letter = letter.lower()
 
 		if (low_letter in vowels):
 
-			# if its a vowel, then
+			# if its a vowel, then incremenet the count for that letter
 			vowels[low_letter] = vowels[low_letter]+1
 
 
+# only problem is the ordering of the list, would be better if a, e, i, o, u
 for vowel, count in vowels.items():
 
 	upper_vowel = vowel.upper()
